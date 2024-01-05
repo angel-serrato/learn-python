9fae8e171c1a0da010bd2ee3afbb035c7795d8c7
Angel Serrato <serratoarias@yahoo.com>
2024-01-04 23:12:50 -0500

    Adding day 2

---
+++ A Beginner/Day-2-Tip-Calculator/notes.py
@@ -0,0 +1,49 @@
+# Data types, variables, strings.
+"""
+Data Types => String
+"""
+# this is called subscripting => pulling out a particular element of the string
+print('Hello'[4]) # it returns the first character of the string
+"""
+Integer
+"""
+print("123" + "345")
+print(123 + 345)
+# to visualize numbers more easily we add _ 
+print(123_456_798)
+"""
+Float or floating point number
+"""
+3.14159
+"""
+Boolean
+"""
+True
+False
+"""
+type function checking
+"""
+num_char = len(input("What is your name?\n"))
+print(type(num_char))
+"""
+str function convertion
+"""
+new_num_char = str(num_char)
+print('Your name has ' + new_num_char + ' characters.')
+"""
+Mathematical operatos
+"""
+3 + 5
+7 - 4
+3 * 5
+# when you are dividing you will end up with a floating number
+40 / 5
+2 ** 3
+# PEMDAS This calculation goes from left to right
+# Parentheses
+# Exponents
+# Multiplication
+# Division
+# Addition
+# Substraction
+
