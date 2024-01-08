f9b5f7c3b550b7e63489a9f8dd2d2db70a9db4ad
Angel Serrato <serratoarias@yahoo.com>
2024-01-08 18:42:02 -0500

    Adding day 5

---
+++ A Beginner/Day-5-PyPassword-Generator/main.py
@@ -0,0 +1,14 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print('Welcome to the PyPassword Generator!')
+letras = int(input('How many letters would you like in your password?\n'))
+# simbolos = input('How many symbols would you like?\n')
+# numeros = input('How many numbers would you like?\n')
+password = ""
+for item in range(0, letras + 1):
+    azar = random.randint(0, letras)
+    print(letters[azar])
+# print('Here is your password: ')
