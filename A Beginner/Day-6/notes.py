62b564e735b879c2eb338bc67d22a15790dc0a04
Angel Serrato <serratoarias@yahoo.com>
2024-01-11 22:02:35 -0500

    Adding day 6

---
+++ A Beginner/Day-6/notes.py
@@ -0,0 +1,17 @@
+"""
+Functions
+len() range() print()
+def => defining a function
+"""
+def my_function():
+    print('Hello from a function')
+
+my_function()
+"""
+Arguments or parameters
+Remember to execute the function or call the function
+"""
+def greeting(f_name, l_name):
+    print(f'Hello {f_name} {l_name}')
+
+greeting('Angel', 'Serrato')
