35d36964a064b41fd7ec72e986b73acea1d2a2c3
Angel Serrato <serratoarias@yahoo.com>
2024-01-20 21:43:55 -0500

    Doing day 7

--- A Beginner/Day-7-Hangman/main.py
+++ A Beginner/Day-7-Hangman/main.py
@@ -5,8 +5,10 @@ word_list = ['angel', 'didier', 'serrato','arias', 'gato', 'perro', 'mono', 'ave
 
 # Todo 1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 
-number = random.randint(0, (len(word_list) - 1))
-chosen_word = word_list[number]
+chosen_word = random.choice(word_list)
+
+# number = random.randint(0, (len(word_list) - 1))
+# chosen_word = word_list[number]
 
 # Todo 2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
@@ -14,8 +16,30 @@ guess = input("Guess a letter: ").lower()
 
 # Todo 3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
         
-for letter in chosen_word:
-    if letter == guess:
-        print('right')
-    else:
-        print('wrong')
+# for letter in chosen_word:
+#     if letter == guess:
+#         print('right')
+#     else:
+#         print('wrong')
+
+# Step 2
+        
+# Todo 1 - Create an empty list called display.
+# For each letter in the chosen_word, add a "_" to 'display'.
+# So if the chosen_word was 'apple', display should be ["_","_","_","_","_"].
+        
+print(f'This is the solution {chosen_word}')
+display = []
+for item in chosen_word:
+    # display.append("_")
+    display += "_"
+print(display)
+
+"""
+Todo 2 - Loop through each position in the chosen_word; If the letter at that position
+matches 'guess' then reveal that letter in the display at that position.
+"""
+
+# for letter in chosen_word:
+#     if letter == guess:
+#         display[]
