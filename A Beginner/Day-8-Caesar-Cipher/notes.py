bc54403042b798b20b6a92f830a7f8b082864e23
Angel Serrato <serratoarias@yahoo.com>
2024-01-26 22:00:27 -0500

    Day 8

---
+++ A Beginner/Day-8-Caesar-Cipher/notes.py
@@ -0,0 +1,20 @@
+"""
+Simple function
+Create a function called greet().
+Write 3 print statements inside the function.
+Call the greet function and run your code.
+"""
+
+def greet():
+    print("Hi Angel")
+
+greet()
+
+"""
+Function that allows for input
+"""
+
+def greating(name):
+    print(f"Hola {name}")
+
+greating("Angela")
