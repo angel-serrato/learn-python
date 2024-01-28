fc31c4b885c73b3b9231f55e25ebe470d4a0ee3b
Angel Serrato <serratoarias@yahoo.com>
2024-01-27 22:05:07 -0500

    day 8

--- A Beginner/Day-8-Caesar-Cipher/notes.py
+++ A Beginner/Day-8-Caesar-Cipher/notes.py
@@ -18,3 +18,24 @@ def greating(name):
     print(f"Hola {name}")
 
 greating("Angela")
+
+"""
+Function with more than one input
+We have to be aware of the position parameter
+"""
+
+def greating(name, lastname):
+    print(f"Hola {name} {lastname}")
+
+greating("Angela","Serrato")
+
+"""
+Moving around the parameters
+"""
+
+def calculate(a, b, c):
+    print(a)
+    print(b)
+    print(c)
+
+calculate(c=1, b=2, a=3)
