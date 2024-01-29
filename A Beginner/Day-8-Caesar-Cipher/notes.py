c101a94f5e7a775d4d28723d67ad3a65d4805865
Angel Serrato <serratoarias@yahoo.com>
2024-01-28 19:13:31 -0500

    Day 8

--- A Beginner/Day-8-Caesar-Cipher/notes.py
+++ A Beginner/Day-8-Caesar-Cipher/notes.py
@@ -39,3 +39,37 @@ def calculate(a, b, c):
     print(c)
 
 calculate(c=1, b=2, a=3)
+
+"""
+Cans of paint exercise
+"""
+
+# Write your code below this line 👇
+import math
+def paint_calc(height, width, cover):
+  cans = math.ceil((height * width) / cover)
+  print(f'You\'ll need {cans} cans of paint.')
+# Write your code above this line 👆
+# Define a function called paint_calc() so the code below works.
+# 🚨 Don't change the code below 👇
+test_h = int(input()) # Height of wall (m)
+test_w = int(input()) # Width of wall (m)
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
+
+"""
+Prime number
+"""
+
+# Write your code below this line 👇
+def prime_checker(number):
+  for n in range(2, number):
+    if number % n == 0:
+      print('It\'s not a prime number.')
+      return False
+  print('It\'s a prime number.')
+  return True
+# Write your code above this line 👆
+#Do NOT change any of the code below👇
+n = int(input()) # Check this number
+prime_checker(number=n)
