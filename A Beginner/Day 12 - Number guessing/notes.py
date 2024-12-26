2b4515a7bf2a2d81fdf784c0df98a59f9288e638
Angel Serrato <serratoarias@yahoo.com>
2024-12-25 22:00:38 -0500

    Day 12 number guessing game angela yu version

--- A Beginner/Day 12 - Number guessing/notes.py
+++ A Beginner/Day 12 - Number guessing/notes.py
@@ -1,24 +1,43 @@
-# Local and Global scope
-enemies = 1
+from random import randint
+from art import guessing_game
 
-def increase_enemies():
-    # The global statement
-    global enemies
-    enemies += 2
-    print(f"enemies inside function: {enemies}")
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
 
-increase_enemies()
-print(f"enemies outside function {enemies}")
+def check_answer(user_guess, actual_answer, turns):
+    """Checks answer against guess, return the number of turns remaining"""
+    if user_guess > actual_answer:
+        print("Too high.")
+        return turns - 1
+    elif user_guess < actual_answer:
+        print("Too low.")
+        return turns - 1
+    else:
+        print(f"You got it! The answer is {actual_answer}.")
 
-def drink_potion():
-    potion_strength = 2
-    print(potion_strength)
+def set_difficulty():
+    level = input("Choose a difficulty. Type 'easy' or 'hard': ")
+    if level == 'easy':
+        return EASY_LEVEL_TURNS
+    elif level == 'hard':
+        return HARD_LEVEL_TURNS
 
-drink_potion()
-# There is no block scope in python
-# Global constants
-# Cuando estan en mayuscula recuerdas que son constantes con scope global y que no le debes cambiar su valor
-PI = 3.1415
-GOOGLE_URL = "https://www.google.com"
+def game():
+    print(guessing_game)
+    print("Welcome to the number guessing game!")
+    print("I am thinking of a number between 1 and 100.")
+    answer = randint(1, 100)
+    print(f"The correct answer is: {answer}")
+    turns = set_difficulty()
+    guess = 0
+    while guess != answer:
+        print(f"You have {turns} attempts remaining to guess the number.")
+        guess = int(input("Make a guess: "))
+        turns = check_answer(guess, answer, turns)
+        if turns == 0:
+            print("You've run out of guesses. You lose!")
+            return
+        elif guess != answer:
+            print("Guess again.")
 
-
+game()
